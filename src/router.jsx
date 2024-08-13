380337c1f95b044e29a7eaada51788f8f1854f97
Muhammad Ainur Rofiq <rofiq@192.168.1.8>
2024-08-13 18:56:16 +0700

    feat: add mainLayout, homepage, leftbar, navbar

--- src/router.jsx
+++ src/router.jsx
@@ -1,9 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import MainLayout from "./layouts/MainLayout";
 
 export default createBrowserRouter([
     {
-        path: '',
-        element: <HomePage />
+        element: <MainLayout />,
+        children: [
+            {
+                path: '',
+                element: <HomePage />
+            }
+        ]
     }
 ])
