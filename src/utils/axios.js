86f632b7fcb74d2be672a8b7bc876ff44444e219
Muhammad Ainur Rofiq <rofiq@192.168.1.3>
2024-08-15 01:13:03 +0700

    axios config

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const request = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://simple-sosmed-server.ainurrofiq.site",
 })
 
 export default request;
